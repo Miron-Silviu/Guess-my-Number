8ef8f9e56717039fbf534b1f6630ca44ef0ce40b
Silviu <silviu_miron_90@outlook.com>
2025-03-06 16:19:01 +0000

    Feat Implement functionality for score.
    -Creat another if else statement and declare the message  in case when the score is 0 .

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -12,8 +14,25 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 To high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 To high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' 💥You lost the game !';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 To low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 To low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' 💥You lost the game !';
+      document.querySelector('.score').textContent = 0;
+    }
+    // document.querySelector('.message').textContent = '📉 To low!';
+    // score--;
+    // document.querySelector('.score').textContent = score;
   }
 });
