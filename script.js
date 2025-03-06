376690dccb8b953d9db8681a4a88b4d10c67f425
Silviu <silviu_miron_90@outlook.com>
2025-03-06 15:48:08 +0000

    Feat :Implement logic for guess and secret number;
    -Create if else statement for each condition and check if the conditions is true or false.

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,5 +9,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 To high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 To low!';
   }
 });
