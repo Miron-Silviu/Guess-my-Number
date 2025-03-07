6cd3c98576520d0a6b980322852dbb5ee57063ef
Silviu <silviu_miron_90@outlook.com>
2025-03-07 15:56:02 +0000

    Feat : Create functionality for highscore

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
-
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -20,7 +20,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.with = '30rem';
-
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // When guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
